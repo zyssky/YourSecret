f2c942f447ec3e458ecf557bee98446d5339c256
zyssky <2462084760@qq.com>
2017-03-22 23:21:03 +0800

    成功集成apn到app
    修改了property，service启动问题，notification构造问题

--- app/src/main/java/com/example/administrator/yoursecret/MainActivity.java
+++ app/src/main/java/com/example/administrator/yoursecret/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.administrator.yoursecret;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.administrator.yoursecret.client.NotificationService;
+import com.example.administrator.yoursecret.client.ServiceManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Settings
+        Button okButton = (Button) findViewById(R.id.btn_settings);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                ServiceManager.viewNotificationSettings(MainActivity.this);
+            }
+        });
+
+        // Start the service
+        ServiceManager serviceManager = new ServiceManager(this);
+        serviceManager.setNotificationIcon(android.R.drawable.sym_def_app_icon);
+        serviceManager.startService();
     }
 }
