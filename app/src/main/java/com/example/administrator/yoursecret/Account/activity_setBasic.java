27d3db64eb590d3aeb43dfb0c9f24c3afde55270
mkd2333 <1453340008@qq.com>
2017-06-20 17:00:12 +0800

    实现了account界面的跳转，头像的选择裁剪，account界面每项item的初步逻辑

---
+++ app/src/main/java/com/example/administrator/yoursecret/Account/activity_setBasic.java
@@ -0,0 +1,241 @@
+package com.example.administrator.yoursecret.Account;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.administrator.yoursecret.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static java.security.AccessController.getContext;
+
+/**
+ * Created by j on 2017/6/19.
+ */
+
+public class activity_setBasic extends AppCompatActivity implements View.OnClickListener {
+    static final int RG_REQUEST = 0;
+    private Context context;
+    private ImageView touxiang;
+    private LinearLayout nicheng,mima,click_touxiang;
+    private TextView m_nicheng,m_zhanghao,m_mima;
+    String s ;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.account_set);
+        initView();
+        super.onCreate(savedInstanceState);
+
+    }
+
+    private void initView() {
+        touxiang=(ImageView) findViewById(R.id.touxiang2) ;
+        nicheng=(LinearLayout)findViewById(R.id.nic);
+        mima=(LinearLayout)findViewById(R.id.mim) ;
+        touxiang.setOnClickListener(this);
+        nicheng.setOnClickListener(this);
+        mima.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.touxiang2:
+                ShowPickDialog();
+                break;
+            case R.id.nic:
+                Intent intent1=new Intent();
+                intent1.setClass(this,activity_setItem1.class);
+               startActivity(intent1);
+                break;
+            case R.id.mim:
+                Intent intent2=new Intent();
+                intent2.setClass(this,activity_setItem1.class);
+                startActivity(intent2);
+                break;
+
+        }
+
+    }
+
+
+
+    private void ShowPickDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("设置头像")
+                .setNegativeButton("相册", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(Intent.ACTION_PICK, null);
+                        intent.setDataAndType(
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                "image/*");
+                        startActivityForResult(intent, 1);
+                    }
+                })
+                .setPositiveButton("拍照", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(
+                                MediaStore.ACTION_IMAGE_CAPTURE);
+// 下面这句指定调用相机拍照后的照片存储的路径
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri
+                                .fromFile(new File(Environment
+                                        .getExternalStorageDirectory(),
+                                        "xiaoma.jpg")));
+                        startActivityForResult(intent, 2);
+                    }
+                }).show();
+    }
+   @Override
+   public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode != RESULT_CANCELED) {
+
+            switch (requestCode) {
+// 如果是直接从相册获取
+                case 1:
+                    startPhotoZoom(data.getData());
+                    break;
+// 如果是调用相机拍照时
+                case 2:
+                    File temp = new File(Environment.getExternalStorageDirectory()
+                            + "/xiaoma.jpg");
+                    startPhotoZoom(Uri.fromFile(temp));
+                    break;
+// 取得裁剪后的图片
+                case 3:
+                    if (data != null) {
+                        setPicToView(data);
+                    }
+                    break;
+                default:
+                    break;
+
+                }
+
+            }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+
+    public void startPhotoZoom(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+// 下面这个crop=true是设置在开启的Intent中设置显示的VIEW可裁剪
+        intent.putExtra("crop", "true");
+// aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+// outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, 3);
+    }
+    /**
+     * 保存裁剪之后的图片数据
+     *
+     * @param picdata
+     */
+    private void setPicToView(Intent picdata) {
+        Bundle extras = picdata.getExtras();
+        if (extras != null) {
+            Bitmap photo = extras.getParcelable("data");
+            Drawable drawable = new BitmapDrawable(photo);
+            touxiang.setImageDrawable(drawable);
+            saveImage(photo);
+        }
+    }
+    private void saveImage(Bitmap bitmap) {
+        File filesDir;
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){//判断sd卡是否挂载
+            //路径1：storage/sdcard/Android/data/包名/files
+            filesDir = getApplicationContext().getExternalFilesDir("");
+
+        }else{//手机内部存储
+            //路径：data/data/包名/files
+            filesDir = getApplicationContext().getFilesDir();
+
+        }
+        FileOutputStream fos = null;
+        try {
+            File file = new File(filesDir,"icon.png");
+            fos = new FileOutputStream(file);
+
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100,fos);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }finally{
+            if(fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //如果本地有,就不需要再去联网去请求
+
+    private boolean readImage() {
+        File filesDir;
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){//判断sd卡是否挂载
+            //路径1：storage/sdcard/Android/data/包名/files
+            filesDir = getApplicationContext().getExternalFilesDir("");
+
+        }else{//手机内部存储
+            //路径：data/data/包名/files
+            filesDir = getApplicationContext().getFilesDir();
+
+        }
+        File file = new File(filesDir,"icon.png");
+        if(file.exists()){
+            //存储--->内存
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+            touxiang.setImageBitmap(bitmap);
+            return true;
+        }
+        return false;
+    }
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+
+    /**
+     * 判断本地是否有该图片,没有则去联网请求
+     * */
+@Override
+    public void onResume() {
+        super.onResume();
+        if(readImage()){
+            return;
+        }
+    }
+
+}
