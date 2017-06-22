66f69678556a0ceebaefe1303e8e01b6e3b933cf
zyssky <2462084760@qq.com>
2017-06-22 17:14:00 +0800

    sample

--- app/src/main/java/com/example/administrator/yoursecret/TestMyActivity.java
+++ app/src/main/java/com/example/administrator/yoursecret/TestMyActivity.java
@@ -7,11 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.amap.api.maps.MapView;
+import com.example.administrator.yoursecret.AppManager.ApplicationDataManager;
 import com.example.administrator.yoursecret.AppManager.FoundationManager;
+import com.example.administrator.yoursecret.Entity.UserResponse;
 import com.example.administrator.yoursecret.utils.FileUtils;
 import com.example.administrator.yoursecret.utils.FunctionUtils;
 
 import java.io.File;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class TestMyActivity extends AppCompatActivity {
     public static String TAG = TestMyActivity.class.getSimpleName();
@@ -27,5 +35,33 @@ public class TestMyActivity extends AppCompatActivity {
         String targetPath = FileUtils.toRootPath()+ File.separator+"circle.jpg";
         boolean finish = FileUtils.saveAsPng(targetPath,target);
         Log.d(TAG, "onCreate: "+finish);
+
+        ApplicationDataManager.getInstance().getNetworkManager().register()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<UserResponse>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        Log.d(TAG, "onSubscribe: ");
+                    }
+
+                    @Override
+                    public void onNext(@NonNull UserResponse userResponse) {
+                        //设置ui，保存对应数据
+                        //注册,登录，修改 成功后返回的数据不一样的，有的会没有，需要的就跟我说
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        e.printStackTrace();
+                        Log.d(TAG, "onError: ");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        //如果有什么想在完成后做的可以在这里做
+                        Log.d(TAG, "onComplete: ");
+                    }
+                });
     }
 }
